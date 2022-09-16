dfc352964aa3359e2811fd24beac89da95c81963
Ryan Du <ryan_9809@yahoo.com>
2022-09-16 11:37:09 -0400

    code example middle of class

---
+++ Worksheet/Session_1/code/func.m
@@ -0,0 +1,4 @@
+function y = func(x)
+%     y = x.^2-x-2;
+    y = cos(x)+x;
+end
---
+++ Worksheet/Session_1/code/plot_fucntion.m
@@ -0,0 +1,28 @@
+func_implicit = @(x) x.^2-x-2;
+
+%%
+% define x vector
+x = [-3:0.01:3];
+% calcualte y values
+y = func_implicit(x);
+
+%%
+% I'm plotting functions
+plot(x,y); hold on
+plot(x,x)
+% plot(x,zeros(1,length(x)))
+yline(0)
+xline(0)
+
+legend(["Quadratic","Linear"])
+% legend show
+
+xlabel("x")
+ylabel("f(x)")
+title("Practice Plot")
+
+hold off
+
+%%
+figure
+plot(x,y);
