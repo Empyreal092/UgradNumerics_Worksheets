98e32e5fcaefb3855dcf57cb7463a69f0dfaa6b3
Ryan Shìjié Dù <ryan_9809@yahoo.com>
2022-11-11 13:31:00 -0500

    code from session

--- Session_9/code/inverse_iteration.m
+++ Session_9/code/inverse_iteration.m
@@ -9,22 +9,25 @@ A = temp+temp';
 
 x_0 = randn(n,1);
 
+theta = 1;
+
 %%
 [V,Lambda] = eig(A);
-[eval_max,max_i] = max(abs(diag(Lambda)));
+[inverse_max,max_i] = max( 1./(abs(diag(Lambda)-theta)) );
+eval_max = Lambda(max_i,max_i);
 evec_max = V(:,max_i);
 
-eval_sort = sort(abs(diag(Lambda)));
-eval_2ndmax = eval_sort(end-1);
+eval_sort = sort( 1./(abs(diag(Lambda)-theta)) );
+inverse_2ndmax = eval_sort(end-1);
 
 %%
-max_iter = 100;
+max_iter = 40;
 evec_error_save = nan(1,n);
 eval_error_save = nan(1,n);
 
 x_iter = x_0;
 for i = 1:max_iter
-    x_iter = A*x_iter;
+    x_iter = (A-theta*eye(n))\x_iter;
     x_iter = x_iter/norm(x_iter);
     evec_error_save(i) = min(norm(evec_max-x_iter),norm(-evec_max-x_iter));
 
@@ -37,8 +40,8 @@ figure(3)
 iter_ary = 1:max_iter;
 semilogy(iter_ary,evec_error_save,'ro'); hold on
 semilogy(iter_ary,eval_error_save,'bo');
-semilogy(iter_ary,(eval_2ndmax/eval_max).^iter_ary,'k')
-semilogy(iter_ary,(eval_2ndmax/eval_max).^(2*iter_ary)*10,'k--')
+semilogy(iter_ary,(inverse_2ndmax/inverse_max).^iter_ary,'k')
+semilogy(iter_ary,(inverse_2ndmax/inverse_max).^(2*iter_ary)*10,'k--')
 legend(["error($v^k$)","error($\lambda^k$)"])
 
 ylimm = ylim; ylim([1e-18 ylimm(2)])
